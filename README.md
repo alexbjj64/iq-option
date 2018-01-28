fc19cd43573450b4c346aab36528ce2870a99756
alexbjj64 <apukhar@gmail.com>
2018-01-28 23:10:46 +0300

    tests added -- GET POST

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ nvm use
 npm install
 npm start
 ```
+
+
+#optional
+ npm install mocha --save-dev
+ Then we will install chai using --save-dev
+ 
+ npm install chai --save-dev
+ Last dependency we need to install is supertest:
+ 
+ npm install supertest --save-dev
--- test.js
+++ test.js
@@ -4,49 +4,98 @@ const chai = require('chai'),
     chaiHttp = require('chai-http'),
     expect = chai.expect,
     ws = require('./lib/ws'),
-    {login, parseCookies, wait} = require('./lib/utils');
 
-const URI = 'http://localhost:3000',
-    WS_URI = 'ws://localhost:3000/websocket';
+    {login, parseCookies, wait, auth} = require('./lib/utils');
+
+// const URI = 'http://localhost:3000',
+const URI = 'https://auth.iqoption.com/api/v1.0/login',
+    WS_URI = 'ws://localhost:3000/websocket',
+    REG_URI = 'https://iqoption.com/api/register';
+let express = require('express')
 
 chai.use(chaiHttp);
 
 describe('A login API and websocket BDD test', () => {
 
     describe('General checks', () => {
-        it('should be a running server', () => {
+            it('should be a running server', () => {
+                chai.request(URI)
+                    .get('/')
+                    .end((err, res) => {
+                        expect(res.status).to.equal(200);
+                    });
+            });
+
+    });
+
+    describe('Login scenario', () => {
+
+        before(() => {
             chai.request(URI)
                 .get('/')
                 .end((err, res) => {
                     expect(res.status).to.equal(200);
                 });
         });
-    });
-
-    describe('Login scenario', () => {
 
         it('should be able to successfully login', () => {
-            return login(URI, 'test', 'password').then(res => {
+            return login(URI, 'test@shit.ru', 'password').then(res => {
                 expect(res.status).to.equal(200);
                 expect(res.body).to.be.an('object');
                 expect(res.body).to.have.a.property('status');
                 expect(res.body).to.have.a.property('msg');
                 expect(res.body.status).to.be.a('string').and.equal('success');
-                expect(res).to.have.cookie('ssid');        
+                expect(res).to.have.cookie('ssid');
             });
         });
 
-        it('should return 401 in case of unsuccessful login', () => {
-            return login(URI, 'test', 'incorrect').then(null, err => err.response).then(res => {
+
+        it('should return 403 in case of unsuccessful login', () => {
+            return login(URI, 'test@mai.ua', 'incorrect').then(null, err => err.response).then(res => {
                 expect(res.status).to.equal(401);
                 expect(res.body).to.be.an('object');
                 expect(res.body).to.have.a.property('status');
-                expect(res.body).to.not.have.a.property('msg');  
-                expect(res.body.status).to.be.a('string').and.equal('error');            
+                expect(res.body).to.not.have.a.property('msg');
+                expect(res.body.status).to.be.a('string').and.equal('error');
             });
         });
 
     });
+
+    describe('Authorization scenario', () => {
+
+        before(() => {
+            chai.request(URI)
+                .get('/')
+                .end((err, res) => {
+                    expect(res.status).to.equal(200);
+                });
+        });
+
+        it('should be able to successfully auth', () => {
+            return auth(REG_URI, 'defailtName', 'defaultSecondNAme', 'test@shit.ru', 'password').then(res => {
+                expect(res.status).to.equal(200);
+                expect(res.body).to.be.an('object');
+                expect(res.body).to.have.a.property('status');
+                expect(res.body).to.have.a.property('msg');
+                expect(res.body.status).to.be.a('string').and.equal('success');
+                expect(res).to.have.cookie('ssid');
+            });
+        });
+
+
+        it('should return 401 in case of unsuccessful auth', () => {
+            return auth(REG_URI, 'defailtName', 'defaultSecondNAme','test@mai.ua', 'incorrect').then(null, err => err.response).then(res => {
+                expect(res.status).to.equal(401);
+                expect(res.body).to.be.an('object');
+                expect(res.body).to.have.a.property('status');
+                expect(res.body).to.not.have.a.property('msg');
+                expect(res.body.status).to.be.a('string').and.equal('error');
+            });
+        });
+
+    });
+
 
     describe('Websocket subscription', () => {
         let ssid = '';
@@ -85,5 +134,5 @@ describe('A login API and websocket BDD test', () => {
             });
         }
     });
-
 });
+
